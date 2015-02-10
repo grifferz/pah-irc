266ed1f10da1276554ea8e47df7af256650bd87a
Andy Smith <andy-git-pah-irc@strugglers.net>
2015-02-10 07:05:34 +0000

    New schema dump.

--- sql/PAH-Schema-0.0003-SQLite.sql
+++ sql/PAH-Schema-0.0003-SQLite.sql
@@ -1,6 +1,6 @@
 -- 
 -- Created by SQL::Translator::Producer::SQLite
--- Created on Tue Feb 10 05:27:22 2015
+-- Created on Tue Feb 10 07:03:23 2015
 -- 
 
 BEGIN TRANSACTION;
@@ -36,23 +36,6 @@ CREATE TABLE users (
 CREATE UNIQUE INDEX users_nick_idx ON users (nick);
 
 --
--- Table: users_games
---
-DROP TABLE users_games;
-
-CREATE TABLE users_games (
-  id INTEGER PRIMARY KEY NOT NULL,
-  user integer NOT NULL,
-  game integer NOT NULL
-);
-
-CREATE INDEX users_games_user_idx ON users_games (user);
-
-CREATE INDEX users_games_game_idx ON users_games (game);
-
-CREATE UNIQUE INDEX users_games_user_game_idx ON users_games (user, game);
-
---
 -- Table: games
 --
 DROP TABLE games;
@@ -71,5 +54,29 @@ CREATE INDEX games_idx_channel ON games (channel);
 CREATE INDEX games_status_idx ON games (status);
 
 CREATE UNIQUE INDEX games_channel_idx ON games (channel);
+
+--
+-- Table: users_games
+--
+DROP TABLE users_games;
+
+CREATE TABLE users_games (
+  id INTEGER PRIMARY KEY NOT NULL,
+  user integer NOT NULL,
+  game integer NOT NULL,
+  wins integer NOT NULL DEFAULT 0,
+  hands integer NOT NULL DEFAULT 0,
+  tsarcount integer NOT NULL DEFAULT 0,
+  is_tsar integer NOT NULL DEFAULT 0,
+  wait_since integer NOT NULL DEFAULT 0,
+  FOREIGN KEY (game) REFERENCES games(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (user) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE INDEX users_games_idx_game ON users_games (game);
+
+CREATE INDEX users_games_idx_user ON users_games (user);
+
+CREATE UNIQUE INDEX users_games_user_game_idx ON users_games (user, game);
 
 COMMIT;
