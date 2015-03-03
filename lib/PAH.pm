1d86118e5a89c76c803d34b68bee0eb61f3fbef0
Andy Smith <andy-git-pah-irc@strugglers.net>
2015-03-03 20:32:51 +0000

    Use the correct nick in the win message.

--- lib/PAH.pm
+++ lib/PAH.pm
@@ -3579,17 +3579,21 @@ sub announce_winner {
     # with the new Card Tsar.
     my @active_usergames = $game->rel_active_usergames;
 
+    my $winner_nick = $winner->rel_user->disp_nick;
+
+    $winner_nick = $winner->rel_user->nick if (not defined $winner_nick);
+
     foreach my $ug (@active_usergames) {
         # Don't message the current Tsar as presumably they were there when
         # they picked the winner.
         next if ($ug->id == $current_tsar->id);
 
-        my $nick = $ug->rel_user->disp_nick;
-        $nick = $ug->rel_user->nick if (not defined $nick);
+        my $user_nick = $ug->rel_user->disp_nick;
+        $user_nick = $ug->rel_user->nick if (not defined $user_nick);
 
         if ($ug->id == $winner->id) {
             # Congratulate winning user.
-            $irc->msg($nick,
+            $irc->msg($user_nick,
                 sprintf("[%s] Congrats, you won! You now have %u Awesome"
                     . " Point%s! Your winning play was:", $chan, $winner->wins,
                     $winner->wins == 1 ? '' : 's'));
@@ -3598,17 +3602,17 @@ sub announce_winner {
             my $pronoun = $winner->rel_user->pronoun;
             $pronoun = 'their' if (not defined $pronoun);
 
-            $irc->msg($nick,
+            $irc->msg($user_nick,
                 sprintf("[%s] The winner was %s, who now has %u"
                     . " Awesome Point%s! %s winning play was:", $chan,
-                    $nick, $winner->wins, $winner->wins == 1 ? '' : 's',
+                    $winner_nick, $winner->wins, $winner->wins == 1 ? '' : 's',
                     ucfirst($pronoun)));
         }
 
         foreach my $line (split(/\n/, $winplay)) {
             # Sometimes YAML leaves us with a trailing newline in the text.
             next if ($line =~ /^\s*$/);
-            $irc->msg($nick, "→ $line");
+            $irc->msg($user_nick, "→ $line");
         }
     }
 }
